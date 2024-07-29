f47d9c8b4f057ea1251c4d50b93fd55ca1dfec34
hanay0 <ahanafy90@yahoo.com>
2024-07-30 00:41:52 +0300

    implement core modules & components

---
+++ src/app/cart/cart-dropdown/cart-dropdown.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-dropdown',
+  templateUrl: './cart-dropdown.component.html',
+  styleUrl: './cart-dropdown.component.scss'
+})
+export class CartDropdownComponent {
+
+}
---
+++ src/app/cart/cart.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartDropdownComponent } from './cart-dropdown/cart-dropdown.component';
+
+
+
+@NgModule({
+  declarations: [
+    CartDropdownComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CartModule { }
