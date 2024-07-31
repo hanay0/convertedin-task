94e28832bfca94a7f6299a0e8437e570fde9b37d
hanay0 <ahanafy90@yahoo.com>
2024-08-01 02:44:19 +0300

    fix serve issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { HttpClientModule } from '@angular/common/http';
-import { productReducer } from './products/store/product.reducer';
-import { ProductEffects } from './products/store/product.effects';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './core/navbar/navbar.component';
-import { AppRoutingModule } from './app-routing.module';
-import { CommonModule } from '@angular/common';
+// import { NgModule, isDevMode  } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { StoreModule } from '@ngrx/store';
+// import { EffectsModule } from '@ngrx/effects';
+// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+// import { HttpClientModule } from '@angular/common/http';
+// import { productReducer } from './products/store/product.reducer';
+// import { ProductEffects } from './products/store/product.effects';
+// import { AppComponent } from './app.component';
+// import { NavbarComponent } from './layout/navbar/navbar.component';
+// import { AppRoutingModule } from './app-routing.module';
+// import { CommonModule } from '@angular/common';
 // import { ProductListComponent } from './products/product-list/product-list.component';
 // import { SidebarComponent } from './core/navbar/sidebar/sidebar.component';
 // import { ProductDetailsComponent } from './products/product-details/product-details.component';
+
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { CommonModule } from "@angular/common";
+import { EffectsModule } from "@ngrx/effects";
+import { productReducer } from "./products/store/product.reducer";
+import { ProductEffects } from "./products/store/product.effects";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { isDevMode, NgModule } from "@angular/core";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
   declarations: [
@@ -23,7 +38,6 @@ import { CommonModule } from '@angular/common';
     HttpClientModule,
     AppRoutingModule,
     CommonModule,
-    StoreModule.forRoot({ products: productReducer }),
     EffectsModule.forRoot([ProductEffects, ]),
     StoreModule.forRoot({ products: productReducer }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
